b65cb04919194bd1afbabea8bfb506da99fdd1e1
David De Wulf <contact.dewulf@gmail.com>
2019-01-23 09:03:59 -0800

    19 - Updating Items with Queries and Mutations

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -37,7 +37,6 @@ class CreateItem extends React.Component {
 
   handleChange = e => {
     const { name, type, value } = e.target;
-    console.log({ name, type, value });
     const val = type === 'number' ? parseFloat(value) : value;
 
     this.setState({ [name]: val });
@@ -85,61 +84,61 @@ class CreateItem extends React.Component {
             }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
-              <label htmlFor='file'>
+              <label htmlFor="file">
                 Image
                 <input
-                  type='file'
-                  id='file'
-                  name='file'
-                  placeholder='Upload an image'
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
                   required
                   onChange={this.uploadFile}
                 />
               </label>
 
               {this.state.image && (
-                <img width='200' src={this.state.image} alt='Upload preview' />
+                <img width="200" src={this.state.image} alt="Upload preview" />
               )}
 
-              <label htmlFor='title'>
+              <label htmlFor="title">
                 Title
                 <input
-                  type='text'
-                  id='title'
-                  name='title'
-                  placeholder='Title'
+                  type="text"
+                  id="title"
+                  name="title"
+                  placeholder="Title"
                   required
                   value={this.state.title}
                   onChange={this.handleChange}
                 />
               </label>
 
-              <label htmlFor='price'>
+              <label htmlFor="price">
                 Price
                 <input
-                  type='number'
-                  id='price'
-                  name='price'
-                  placeholder='Price'
+                  type="number"
+                  id="price"
+                  name="price"
+                  placeholder="Price"
                   required
                   value={this.state.price}
                   onChange={this.handleChange}
                 />
               </label>
 
-              <label htmlFor='description'>
+              <label htmlFor="description">
                 Description
                 <textarea
-                  id='description'
-                  name='description'
-                  placeholder='Description'
+                  id="description"
+                  name="description"
+                  placeholder="Description"
                   required
                   value={this.state.description}
                   onChange={this.handleChange}
                 />
               </label>
 
-              <button type='submit'>Submit</button>
+              <button type="submit">Submit</button>
             </fieldset>
           </Form>
         )}
